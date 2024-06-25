c6716d05791d48cb28322cbb02d641bf8958de78
Lind <lind.daki@hotmail.com>
2024-06-25 17:13:11 +0200

    d) Listen, Schleifen und bedingte Ausführung hinzugefügt, um mehrere Reservierungen zu verwalten

--- reservierung/src/main/java/ch/App.java
+++ reservierung/src/main/java/ch/App.java
@@ -1,13 +1,28 @@
 package ch;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class App {
     public static void main(String[] args) {
-        Kunde kunde = new Kunde("Max Mustermann", "0123456789");
-        Reservierung reservierung = new Reservierung(kunde, LocalDateTime.now());
+        List<Reservierung> reservierungen = new ArrayList<>();
 
-        System.out.println(kunde);
-        System.out.println(reservierung);
+        
+        reservierungen.add(new Reservierung(new Kunde("Max Mustermann", "0123456789"), LocalDateTime.now()));
+        reservierungen.add(new Reservierung(new Kunde("Erika Mustermann", "0987654321"), LocalDateTime.now().plusDays(1)));
+
+        
+        for (Reservierung reservierung : reservierungen) {
+            System.out.println(reservierung);
+        }
+
+        
+        String suchName = "Max Mustermann";
+        for (Reservierung reservierung : reservierungen) {
+            if (reservierung.getKunde().getName().equals(suchName)) {
+                System.out.println("Gefundene Reservierung: " + reservierung);
+            }
+        }
     }
 }
